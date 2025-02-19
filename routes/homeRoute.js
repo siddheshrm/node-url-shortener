d7866b5f7b57d1ee5140050289f6b6fec71f586c
siddheshrm <siddheshmestri07@gmail.com>
2025-02-19 12:43:11 +0530

    added analytics using EJS

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const URLData = require("../models/urlModel");
+
 const router = express.Router();
 
-router.get("/", (request, response) => {
-  return response.render("homeView");
+router.get("/", async (request, response) => {
+  const allURLs = await URLData.find({});
+
+  return response.render("homeView", {
+    urls: allURLs,
+  });
 });
 
 module.exports = router;
